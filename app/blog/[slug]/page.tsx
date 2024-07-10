c30984ef7aca7d76d7f312e96080327c60aa089d
Yesaswi Vadapalli <yesaswi.vadapalli@gmail.com>
2024-07-09 22:09:18 -0400

    Added OpenGraph and Metadata generation

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,12 +1,59 @@
 import Link from "next/link";
-
 import { notFound } from "next/navigation";
-
-import { getBlogPost, formatDate } from "../utils";
-
+import { getBlogPost, getBlogPosts, formatDate } from "../utils";
 import { CustomMDX } from "app/components/mdx";
+import { baseUrl } from "app/sitemap";
 
 export const dynamic = "force-dynamic";
+
+export async function generateStaticParams() {
+  let posts = await getBlogPosts();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  let post = await getBlogPost(params.slug);
+  if (!post) {
+    return;
+  }
+  let {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+  } = post.metadata;
+  let ogImage = image
+    ? `${baseUrl}${image}`
+    : `${baseUrl}/og?title=${encodeURIComponent(title)}`;
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "article",
+      publishedTime,
+      url: `${baseUrl}/blog/${post.slug}`,
+      images: [
+        {
+          url: ogImage,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [ogImage],
+    },
+  };
+}
 
 export default async function BlogPost({
   params,
@@ -14,27 +61,44 @@ export default async function BlogPost({
   params: { slug: string };
 }) {
   const post = await getBlogPost(params.slug);
-
   if (!post) {
     notFound();
   }
-
   return (
     <section>
+      <script
+        type="application/ld+json"
+        suppressHydrationWarning
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BlogPosting",
+            headline: post.metadata.title,
+            datePublished: post.metadata.publishedAt,
+            dateModified: post.metadata.publishedAt,
+            description: post.metadata.summary,
+            image: post.metadata.image
+              ? `${baseUrl}${post.metadata.image}`
+              : `${baseUrl}/og?title=${encodeURIComponent(post.metadata.title)}`,
+            url: `${baseUrl}/blog/${post.slug}`,
+            author: {
+              "@type": "Person",
+              name: "My Portfolio",
+            },
+          }),
+        }}
+      />
       <h1 className="font-semibold text-2xl mb-4 tracking-tighter">
         {post.metadata.title}
       </h1>
-
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {formatDate(post.metadata.publishedAt, true)}
         </p>
-
         <Link href={`/blog/edit/${post.slug}`} className="text-blue-500">
           Edit Post
         </Link>
       </div>
-
       <article className="prose dark:prose-invert">
         <CustomMDX source={post.content} />
       </article>
