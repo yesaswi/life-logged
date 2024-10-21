39dd647f8df444a269eff03b7d059d9938e292ed
Yesaswi Vadapalli <yesaswi.vadapalli@gmail.com>
2024-10-20 21:03:17 -0400

    Commented out the Homepage Intro

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-3xl font-bold tracking-tighter">Welcome</h1>
-      <p className="mb-6 text-lg">
+      {/* <p className="mb-6 text-lg">
         This is where I chronicle my journey through life, work, and ideas.
         Expect a mix of personal reflections, project insights, and thoughts on
         the books shaping my perspective.
-      </p>
+      </p> */}
       <p className="mb-8 text-lg">
         Whether you're here for inspiration, shared experiences, or simply
         curious about my world, I hope you'll find something that resonates.
